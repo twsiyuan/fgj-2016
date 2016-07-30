3ebe4aeadbb852f3b74b25b764794d34b31a006e
Siyuan Wang <siyuan@twsiyuan.com>
2016-07-30 14:10:15 +0800

    test scene

---
+++ Source/Assets/Scripts/ColliderAutoSize.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ColliderAutoSize : MonoBehaviour {
+	#if UNITY_EDITOR
+	void Update () {
+		var rect = this.GetComponent<RectTransform> ();
+		var collider = this.GetComponent<BoxCollider2D>();
+		if (rect != null && collider != null){
+			collider.size = rect.sizeDelta;
+		}
+	}
+	#endif
+}
---
+++ Source/Assets/Scripts/InitVelocity.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class InitVelocity : MonoBehaviour {
+
+	public Vector2 velocity;
+
+	// Use this for initialization
+	void Start () {
+		var rigi = this.GetComponent<Rigidbody2D> ();
+		rigi.velocity = this.velocity;
+	}
+}
