614c06458394488719445b393d830279e2cb5d68
Siyuan <easy0519@gmail.com>
2016-07-30 18:45:17 +0800

    controller move update, add ground tracking

--- Source/Assets/Scripts/CharacterMovement.cs
+++ Source/Assets/Scripts/CharacterMovement.cs
@@ -3,33 +3,46 @@ using System.Collections;
 
 public class CharacterMovement : MonoBehaviour
 {
-	public Vector2 velocity;
+	readonly Vector2 groundRay = new Vector2 (0, -1);
+	const float groundDistance = 800;
+	int layerMask = 0;
 
-	private Collider2D coll;
-	private float x, y;
+	[Header("Speed")]
+	[SerializeField]
+	float speed = 80;
+
+	[SerializeField]
+	bool run = false;
+
 
 	void Awake ()
 	{
-		coll = GetComponent <BoxCollider2D> ();
-		x = transform.localPosition.x;
-		y = transform.localPosition.y;
-	}
-
-	void OnTriggerEnter2D (Collider2D other)
-	{
-		Debug.Log ("Triggered");
-		velocity *= -1;
-		//transform.localPosition = new Vector3 (x + velocity, y, 0);
+		this.layerMask = LayerMask.GetMask ("Ground");
 	}
 
 	void Update ()
 	{
-		x = transform.localPosition.x;
-		/*if (coll.isTrigger)
+		var trans = transform as RectTransform;
+
+		// Move
+		if (this.run) {
+			var dx = this.speed * Time.deltaTime;
+			var pos = trans.anchoredPosition;
+			pos.x += dx;
+			trans.anchoredPosition = pos;
+		}
+
+		// Set to ground
 		{
-			x *= -1;
-			Debug.Log ("Triggered");
-		}*/
-		transform.localPosition = new Vector3 (x + velocity.x * Time.deltaTime, y + velocity.y * Time.deltaTime, 0);
+			var pos = trans.anchoredPosition + new Vector2( 0, trans.rect.height);
+			var dir = groundRay;
+
+			var hitInfo = Physics2D.Raycast (pos, dir, groundDistance, this.layerMask);
+			if (hitInfo.collider != null) {
+				var cpos = pos + dir * hitInfo.distance;
+				//var cpos = hitInfo.collider.bounds.ClosestPoint (new Vector3 (pos.x, pos.y, 0));
+				trans.anchoredPosition3D = cpos;
+			}
+		}
 	}
 }
