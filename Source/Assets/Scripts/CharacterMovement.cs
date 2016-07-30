01600bb29babf870318d6c16016f893cacaebc66
Siyuan <easy0519@gmail.com>
2016-07-30 19:23:03 +0800

    add char and update char controller

--- Source/Assets/Scripts/CharacterMovement.cs
+++ Source/Assets/Scripts/CharacterMovement.cs
@@ -11,13 +11,38 @@ public class CharacterMovement : MonoBehaviour
 	[SerializeField]
 	float speed = 80;
 
+	[Header("System")]
+	[SerializeField]
+	float physicalThreshold = 10f;
+
+	[SerializeField]
+	float physicalGravityScale = 10;
+
 	[SerializeField]
 	bool run = false;
 
+	public bool PhysicalMode
+	{
+		get{ return this.physicalMode;}
+		set{
+			this.physicalMode = value;
+			this.GetComponent<Rigidbody2D>().gravityScale = value ? this.physicalGravityScale : 0;
+		}
+	}
+
+	bool physicalMode = true;
 
 	void Awake ()
 	{
 		this.layerMask = LayerMask.GetMask ("Ground");
+	}
+
+	void Start()
+	{
+		var footPoint = Vector2.zero;
+		var footDist = 0f;
+		this.ComputeDistance (this.transform as RectTransform, out footPoint, out footDist );
+		this.PhysicalMode = footDist >= this.physicalThreshold;
 	}
 
 	void Update ()
@@ -25,7 +50,7 @@ public class CharacterMovement : MonoBehaviour
 		var trans = transform as RectTransform;
 
 		// Move
-		if (this.run) {
+		if (this.run && !this.PhysicalMode) {
 			var dx = this.speed * Time.deltaTime;
 			var pos = trans.anchoredPosition;
 			pos.x += dx;
@@ -34,15 +59,27 @@ public class CharacterMovement : MonoBehaviour
 
 		// Set to ground
 		{
-			var pos = trans.anchoredPosition + new Vector2( 0, trans.rect.height);
-			var dir = groundRay;
+			var footPoint = Vector2.zero;
+			var footDist = 0f;
+			this.ComputeDistance (this.transform as RectTransform, out footPoint, out footDist );
 
-			var hitInfo = Physics2D.Raycast (pos, dir, groundDistance, this.layerMask);
-			if (hitInfo.collider != null) {
-				var cpos = pos + dir * hitInfo.distance;
-				//var cpos = hitInfo.collider.bounds.ClosestPoint (new Vector3 (pos.x, pos.y, 0));
-				trans.anchoredPosition3D = cpos;
+			this.PhysicalMode = footDist >= this.physicalThreshold;
+			if (!physicalMode) {
+				trans.anchoredPosition = footPoint;
 			}
 		}
+	}
+
+	void ComputeDistance(RectTransform trans, out Vector2 footPoint, out float footDistance)
+	{
+		var height = trans.rect.height;
+		var pos = trans.anchoredPosition + new Vector2( 0, height);
+		var dir = groundRay;
+
+		var hitInfo = Physics2D.Raycast (pos, dir, groundDistance, this.layerMask);
+		var dist = hitInfo.collider == null ? 0 : hitInfo.distance;
+
+		footPoint = pos + dir * dist;
+		footDistance = hitInfo.collider == null ? float.PositiveInfinity : dist - height;
 	}
 }
