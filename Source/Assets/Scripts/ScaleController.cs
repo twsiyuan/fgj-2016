02ce2547efbd10ba18f0a149447b176ced8e9161
Siyuan Wang <siyuan@twsiyuan.com>
2016-07-30 16:19:50 +0800

    add scale controller, and remove zoom input

---
+++ Source/Assets/Scripts/ScaleController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleController : ValueController {
+
+	public Vector3 startScale = new Vector3 (1, 1, 1);
+	public Vector3 endScale = new Vector3(10, 10, 10);
+
+	float t = 0;
+
+	public override float Value
+	{
+		get{ return this.t; }
+		set{ 
+			this.t = Mathf.Clamp01(value);
+		
+			this.transform.localScale = Vector3.Lerp (this.startScale, this.endScale, this.t);
+		}
+	}
+
+	void Reset(){
+		this.startScale = this.transform.localScale;
+	}
+		
+}
