ae8c3dc3816fddc1a4d8a6aff83e19223df3d94f
hubert322 <hubert322@gmail.com>
2016-07-30 15:14:35 +0800

    implemented character movement scene and script

---
+++ Source/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour
+{
+	public float velocity;
+
+	private Collider2D coll;
+	private float x, y;
+
+	void Awake ()
+	{
+		coll = GetComponent <BoxCollider2D> ();
+		x = transform.localPosition.x;
+		y = transform.localPosition.y;
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		Debug.Log ("Triggered");
+		velocity *= -1;
+		//transform.localPosition = new Vector3 (x + velocity, y, 0);
+	}
+
+	void Update ()
+	{
+		x = transform.localPosition.x;
+		/*if (coll.isTrigger)
+		{
+			x *= -1;
+			Debug.Log ("Triggered");
+		}*/
+		transform.localPosition = new Vector3 (x + velocity, y, 0);
+	}
+}
