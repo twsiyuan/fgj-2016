8fcfd62bbc7c6cc94fe8b4c5caf1d0313a554d36
hubert322 <hubert322@gmail.com>
2016-07-30 17:16:14 +0800

    all sliders

---
+++ Source/Assets/Scripts/PositionController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionController : ValueController
+{
+	public Vector3 endPos;
+
+	private float t = 0;
+	private Vector3 startPos;
+
+	void Awake ()
+	{
+		startPos = transform.localPosition;
+	}
+
+	public override float Value
+	{
+		get
+		{
+			return t;
+		}
+
+		set
+		{
+			t = Mathf.Clamp01 (value);
+			transform.localPosition = Vector3.Lerp (startPos, endPos, t);
+		}
+	}
+
+	void Reset ()
+	{
+		endPos = transform.localPosition;
+	}
+}
---
+++ Source/Assets/Scripts/RotationController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RotationController : ValueController
+{
+	public float endRotate;
+
+	private float t = 0, startRotate;
+
+	void Awake ()
+	{
+		startRotate = transform.localRotation.eulerAngles.z;
+	}
+
+	public override float Value
+	{
+		get
+		{
+			return t;
+		}
+
+		set
+		{
+			t = Mathf.Clamp01 (value);
+			Quaternion start = Quaternion.Euler (new Vector3 (0, 0, startRotate));
+			Quaternion end = Quaternion.Euler (new Vector3 (0, 0, endRotate));
+			transform.localRotation = Quaternion.Lerp (start, end, t);
+		}
+	}
+
+	void Reset ()
+	{
+		endRotate = transform.localRotation.eulerAngles.z;
+	}
+}
