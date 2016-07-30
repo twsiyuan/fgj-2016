0295d66b3ecb1b6af6bc4e6dfc5063b088558464
Siyuan Wang <siyuan@twsiyuan.com>
2016-07-30 16:42:12 +0800

    add level data

---
+++ Source/Assets/Editor/LevelDataEditor.cs
@@ -0,0 +1,59 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+[CustomEditor(typeof(LevelData))]
+[CanEditMultipleObjects]
+public class ShopMenuEditor : Editor
+{
+	ReorderableList list;
+
+	void OnEnable()
+	{
+		var serProp = this.serializedObject.FindProperty("levelNames");
+		this.list = new ReorderableList(serProp.serializedObject, serProp, true, true, true, true);
+		this.list.drawElementCallback = this.DrawListElement;
+		this.list.drawHeaderCallback = this.DrawListHeader;
+	}
+
+	void DrawListHeader(Rect rect)
+	{
+		var spacing = 10f;
+		var arect = rect;
+		arect.height = EditorGUIUtility.singleLineHeight;
+
+		arect.x += 15;
+		arect.width = 100;
+		EditorGUI.LabelField(arect, "Name");
+		arect.x += arect.width + spacing ;
+
+	}
+
+	void DrawListElement(Rect rect, int index, bool isActive, bool isFocused)
+	{
+		var spacing = 10f;
+		var arect = rect;
+		var serElem = this.list.serializedProperty.GetArrayElementAtIndex(index);
+		arect.height = EditorGUIUtility.singleLineHeight;
+
+		arect.width = 100;
+		EditorGUI.PropertyField(arect, serElem, GUIContent.none);
+		arect.x += arect.width + spacing;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		this.serializedObject.Update();
+
+		var property = list.serializedProperty;
+		property.isExpanded = EditorGUILayout.Foldout(property.isExpanded, property.displayName);
+		if (property.isExpanded)
+		{
+			this.list.DoLayoutList();
+		}
+
+		this.serializedObject.ApplyModifiedProperties();
+	}
+}
---
+++ Source/Assets/Scripts/LevelData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "Levels" , menuName = "TeamA/Data Setting" , order = 1)]
+public class LevelData : ScriptableObject 
+{
+	public string[] LevelNames{
+		get{ return this.levelNames; }
+	}
+
+	public string GetNextLevel(string name){
+		var index = this.GetLevelIndex (name);
+		return this.GetNextLevel (index);
+	}
+
+	public string GetNextLevel(int index){
+		if (index + 1 >= this.levelNames.Length || index + 1 < 0) {
+			return null;
+		}
+		return this.levelNames[index + 1];
+	}
+
+	public int GetLevelIndex(string name){
+		return System.Array.IndexOf (this.levelNames, name);
+	}
+
+	[HideInInspector]
+	[SerializeField]
+	string[] levelNames = new string[0];
+}
