4d62411bbb03f4b1aa9a1afb49d53a2feac18d23
Siyuan <easy0519@gmail.com>
2016-07-31 12:52:52 +0800

    add player trigger

---
+++ Source/Assets/Scripts/PlayerTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTrigger : MonoBehaviour {
+
+	public Behaviour[] targets = new Behaviour[0];
+	public bool doEn = true;
+
+	void OnTriggerEnter2D (Collider2D c){
+		if (c.gameObject.layer == LayerMask.NameToLayer( "Player")) {
+			foreach (var bev in targets) {
+				bev.enabled = doEn;
+			}
+		}
+	}
+}
