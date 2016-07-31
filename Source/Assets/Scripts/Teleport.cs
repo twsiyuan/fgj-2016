50307ea372651eea570c3535b141dcdcd43f11d3
Siyuan Wang <siyuan@twsiyuan.com>
2016-07-31 12:21:29 +0800

    teleport

--- Source/Assets/Scripts/Teleport.cs
+++ Source/Assets/Scripts/Teleport.cs
@@ -5,13 +5,15 @@ using System.Collections;
 public class Teleport : MonoBehaviour {
 
 	public GameObject there; // 要移動到的目的地
+	public bool doPlayer = true;
+	public bool doMonster = true;
 
 	void OnTriggerEnter2D (Collider2D c){
 	if (this.enabled) {
-			if (c.gameObject.layer == LayerMask.NameToLayer ("Player") ||
-				c.gameObject.layer == LayerMask.NameToLayer ("Monster")) {
+			if ((doPlayer && c.gameObject.layer == LayerMask.NameToLayer ("Player")) ||
+				(doMonster && c.gameObject.layer == LayerMask.NameToLayer ("Monster"))) {
 				c.transform.position = this.there.transform.position;
-			}
+			}	
 		}
 	}
 
