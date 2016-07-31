5a28c71b730c6498e31fc3dd0aa20645396fa72c
Siyuan Wang <siyuan@twsiyuan.com>
2016-07-31 09:43:47 +0800

    add simple cam

---
+++ Source/Assets/Scripts/SimpleCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Camera))]
+public class SimpleCamera : MonoBehaviour {
+
+	[SerializeField]
+	Transform target;
+
+	Camera cam;
+
+	void Awake(){
+		cam = this.GetComponent<Camera> ();
+	}
+
+	void Update () {
+		var pos = cam.transform.position;
+		pos.x = target.transform.position.x;
+		cam.transform.position = pos;
+	}
+}
