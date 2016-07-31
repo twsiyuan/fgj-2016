1ae9291a8a85d50473e3e29cf4554e0621bbb83d
hubert322 <hubert322@gmail.com>
2016-07-31 13:59:35 +0800

    stage 01

---
+++ Source/Assets/Scripts/SliderReset.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderReset : MonoBehaviour
+{
+	public Slider slider;
+	private Vector3 pos;
+	private RectTransform trans;
+	private bool start, end;
+
+	void Awake ()
+	{
+		trans = slider.GetComponent<RectTransform> ();
+		pos = trans.localPosition;
+		start = end = false;
+	}
+
+	void OnTriggerEnter2D (Collider2D coll)
+	{
+		//Debug.Log ("coll");
+		if (coll.name == "BoxStart" || coll.name == "WaterStart")
+		{
+			start = true;
+			Debug.Log (start);
+			slider.value = 0f;
+			trans.localPosition = pos;
+			//Debug.Log ("slider: " + slider.value);
+		}
+		if (coll.name == "BoxEnd" || coll.name == "WaterEnd")
+		{
+			start = false;
+			slider.value = 0f;
+			trans.localPosition = pos;
+			//Debug.Log ("slider: " + slider.value);
+		}
+	}
+
+	void Update ()
+	{
+		if (!start)
+		{
+			slider.interactable = false;
+		}
+		else
+		{
+			slider.interactable = true;
+		}
+	}
+}
