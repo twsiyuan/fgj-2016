5d867044c98cd071d85d3c39209f3ead8f6dd27a
hubert322 <hubert322@gmail.com>
2016-07-31 13:59:35 +0800

    stage 01

---
+++ Source/Assets/Scripts/ObstacleMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ObstacleMove : MonoBehaviour
+{
+	public Slider slider;
+	public Sprite box, stair;
+	void Update ()
+	{
+		if (slider.value >= 0.9)
+		{
+			GetComponent<EdgeCollider2D> ().enabled = true;
+			GetComponent<EdgeCollider2D> ().offset = new Vector2 (-60, -180);
+			GetComponent <BoxCollider2D> ().enabled = false;
+			GetComponent<Image> ().sprite = stair;
+		}
+		else
+		{
+			GetComponent<EdgeCollider2D> ().enabled = false;
+			GetComponent<BoxCollider2D> ().enabled = true;
+			GetComponent<Image> ().sprite = box;
+		}
+	}
+}
---
+++ Source/Assets/Scripts/WaterTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterTrigger : MonoBehaviour
+{
+
+	void Update ()
+	{
+		//Debug.Log ("Scale: " + GetComponent<RectTransform> ().localScale.x);
+		if (GetComponent <RectTransform> ().localScale.x <= 0.35)
+			GetComponent<BoxCollider2D> ().enabled = false;
+		else
+		{
+			GetComponent<BoxCollider2D> ().enabled = true;
+		}
+	}
+}
